b9252eccf3be30ea20ecf53dfc01a5f6441af90a
Arina Shmodina <ApelsinGilka@yandex.ru>
2025-12-08 22:20:23 +0300

    add css and js

---
+++ script.js
@@ -0,0 +1,36 @@
+phrases = [
+    "поиграю в хср",
+    "фаенона качат",
+    "помолюсь танатосу",
+    "посмотрю в потолок",
+    "посмотрю на мидея"
+];
+
+/**
+ * Случайная фраза
+ * @param {Array} arr  - array
+ * @returns {Number} - phrases
+ */
+
+const getRandomPhrase = (arr) => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+};
+
+let randomPhrase = getRandomPhrase(phrases);
+console.log(randomPhrase);
+
+// console.log(document.querySelector(".header"));
+// console.log(document.querySelector("#advice-text"));
+// console.log(document.querySelectorAll(".button"));
+// const a = Array.from(document.querySelectorAll(".button"));
+// console.log(a);
+
+const button = document.querySelector(".button");
+const paragraph = document.querySelector("advice__paragraph");
+const advice = document.querySelector("advice");
+const image = document.querySelector("image");
+
+const hello = () => console.log("hello 42");
+console.dir(paragraph);
+button.addEventListener("click", hello);
